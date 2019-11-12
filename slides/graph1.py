96b81d30c7ceec089a5f9df5410af0aaf4860277
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-11-12 18:25:23 -0400

    Stats about atacks

---
+++ slides/graph1.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+
+table=pickle.load(open("table.p","rb"))
+
+xs=table["Year"][:-2]
+
+ys1=table["XSS"][:-2]
+ys2=table["Sql Injection"][:-2]
+ys3=table["Overflow"][:-2]
+
+data=list(zip(xs,ys1,ys2,ys3))
+
+df=pd.DataFrame(data,columns=["Ano","XSS","SQL Injection","Buffer Overflow"])
+
+df.set_index("Ano").plot()
+
+plt.savefig("no_ataques.pdf",format="pdf",dpi=300)
